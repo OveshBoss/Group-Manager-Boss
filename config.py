1a20a73c1a843d335f01fbdffde7001a3f6d31b7
ovesh boss <oveshboss.08@gmail.com>
2025-12-23 17:28:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Fixed owner/channel
+OWNER_ID = 1416433622
+CHANNEL_URL = "https://t.me/OnlyBossMoviesGroup"

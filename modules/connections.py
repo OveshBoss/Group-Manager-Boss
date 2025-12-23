20e7185401cb7ef4d419a5f60133827b36b33480
ovesh boss <oveshboss.08@gmail.com>
2025-12-23 18:06:45 +0530

    Update connections.py

--- modules/connections.py
+++ modules/connections.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
-from bot import app
 
-@app.on_message(filters.command("connect") & filters.admin)
-async def connect_cmd(_, m):
-    await m.reply("Connect placeholder")
+def init(app):
+    @app.on_message(filters.command("connect") & filters.admin)
+    async def connect_cmd(_, m):
+        await m.reply("Connect placeholder")

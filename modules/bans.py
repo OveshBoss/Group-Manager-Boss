6964f8f9ae961a3e08cd5d904197d33041923ab4
ovesh boss <oveshboss.08@gmail.com>
2025-12-23 18:02:29 +0530

    Update bans.py

--- modules/bans.py
+++ modules/bans.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
-from bot import app
 
-@app.on_message(filters.command("ban") & filters.admin)
-async def ban(_, m):
-    if m.reply_to_message:
-        await app.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
-        await m.reply("User banned ✅")
+def init(app):
+    @app.on_message(filters.command("ban") & filters.admin)
+    async def ban(_, m):
+        if m.reply_to_message:
+            await app.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+            await m.reply("User banned ✅")

220c4d83672413d763ecfa172373c46ac5e8c056
ovesh boss <oveshboss.08@gmail.com>
2025-12-23 17:29:11 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+from config import MONGO_URI
+
+mongo = MongoClient(MONGO_URI)
+db = mongo['mega_bot']
+
+# Collections
+fsub = db['fsub']
+bans = db['bans']
+warnings = db['warnings']
+filters_db = db['filters']
+locks = db['locks']
+welcome_db = db['welcome']
+rules_db = db['rules']
+connections_db = db['connections']
